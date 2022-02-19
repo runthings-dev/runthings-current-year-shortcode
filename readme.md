7de88410796b66de1e46ab976bf934da952a13d1
Matthew Harris <rtpharry@hotmail.com>
2022-02-19 16:32:16 +0000

    chore(release): deploy v1.1.0

---
+++ readme.md
@@ -0,0 +1,44 @@
+# Introduction
+This is a plugin which adds a simple shortcode to display a dynamic
+year, for use in copyright statements.
+
+Its most basic usage is [year], for the current year, but it will expand
+into a date range if the 'from' year is before the current year.
+
+# Usage
+Install the plugin and activate it.
+
+Set "from" to apply a range, after "from" year has passed.
+
+// assuming current year is 2022
+[year] = 2022
+
+// assuming current year is 2022
+[year from="2022"] = 2022
+
+// assuming current year is 2022
+[year from="1983"] = 1983-2022
+
+# Download
+Download and contribute issues at:
+
+https://github.com/rtpHarry/EmptyMediaTitle-Wordpress/
+
+# Changelog
+1.1.0 - 19th February 2022
+  - Added licence
+  - Updated plugin meta
+  - Added readme file
+  - Initial public release
+
+1.0.0 - 25th August 2021
+  - Internal release
+
+# Licence
+This plugin is licenced under GPL 3, and is free to use on personal and 
+commercial projects.
+
+# Author
+Built by Matthew Harris of runthings.dev, copyright 2022.
+
+https://runthings.dev/
---
+++ runthings-current-year-shortcode.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: Current Year Shortcode
+Plugin URI: https://runthings.dev
+Description: Add a shortcode for displaying the current year as a range, usage: [year from="2022"]
+Version: 1.1.0
+Author: Matthew Harris, runthings.dev
+Author URI: https://runthings.dev/
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Copyright 2022 Matthew Harris
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+* Shortcode to display current year in a copyright statement.
+*
+* Set "from" to apply a range, after "from" year has passed.
+*
+* @example
+* // assuming current year is 2022
+* [year] = 2022
+* @example
+* // assuming current year is 2022
+* [year from="2022"] = 2022
+* @example
+* // assuming current year is 2022
+* [year from="1983"] = 1983-2022
+*
+* @return {string} the current year or year range specified
+*/
+function rtp_copyright_year($atts)
+{
+    $atts = shortcode_atts(
+        array(
+            'from' => null,
+        ),
+        $atts,
+        'year'
+    );
+    $year = date('Y');
+    $from = $atts['from'];
+    if ($from != null && $from < $year) {
+        return "$from-$year";
+    }
+    return $year;
+}
+add_shortcode('year', 'rtp_copyright_year');
+// END shortcode to add current year feature
